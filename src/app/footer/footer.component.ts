bd06be9e2633d67becee101f6b0ec375bbca599e
unknown <abdelsadou6@gmail.com>
2022-04-06 14:26:28 +0100

    insertion du footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faTwitter,  faFacebookF, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  faFacebook = faFacebookF;
+  faLinkedin = faLinkedin;
+  faGithub = faGithub;
   constructor() { }
 
   ngOnInit(): void {
